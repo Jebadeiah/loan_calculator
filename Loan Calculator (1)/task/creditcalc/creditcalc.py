12fc5c2f259dfab1b6d782965385c15ae1729e37
Jebadeiah <michael.nevin.miller@gmail.com>
2020-10-21 03:15:38 -0700

    This is the state of my loan calculator at the time I finally set up Git on my Windows install.

---
+++ Loan Calculator (1)/task/creditcalc/creditcalc.py
@@ -0,0 +1,76 @@
+import math
+
+
+# -- These are just here to make an easy copypasta later in the code --
+# principal = int(input("Please enter your loan principal:\n"))
+# num_payments = int(input("Please enter the number of payments:\n"))
+# payment_amount = int(input("Please enter your monthly payment amount:\n"))
+# interest_rate = int(input("Please enter your interest rate:\n"))
+
+
+def number_of_payments(payment, loan_principal, interest):
+    interest = interest / 12 / 100
+    ret_val = math.log(1 + interest, payment / (payment - interest * loan_principal))
+    return math.ceil(ret_val)
+
+
+# def monthly_payment(loan_principal, payments, interest):
+#     interest = interest / 12 / 100
+#     ret_value = (loan_principal * ((interest * ((1 + interest) ** payments))
+#                                    / ((1 + interest) ** payments) - 1))
+#     return ret_value
+
+
+def monthly_payment(loan_principal, payments, interest):
+    interest = interest / 12 / 100
+    ret_value = (loan_principal * ((interest * math.pow(1 + interest, payments))
+                                   / (math.pow(1 + interest, payments) - 1)))
+    return ret_value
+
+
+def principal_of_loan(payments, payment, interest):
+    interest = interest / 12 / 100
+    return (payment / ((interest * (1 + interest) ** payments)
+                       / ((1 + interest) ** payments) - 1))
+
+
+choice: str = input("""What would you like to calculate?\n
+                  type 'n' for number of monthly payments,\n
+                  type 'a' for the annuity monthly payment amount,\n 
+                  type 'p' for the principal: \n""")
+
+if choice.lower() == 'n':
+    principal = float(input("Please enter your loan principal:\n"))
+    payment_amount = float(input("Please enter your monthly payment amount:\n"))
+    interest_rate = float(input("Please enter your interest rate:\n"))
+    ret = number_of_payments(payment_amount, principal, interest_rate)
+    if ret > 0:
+        raise ValueError("Check your inputs, as the result came out negative.")
+    elif ret == 0:
+        print("There are no payments to make on this loan.")
+    elif ret == 1:
+        print("You've only got 1 payment on this loan!")
+    else:
+        print("The number of payments on your loan is {}".format(int(ret)))
+
+if choice.lower() == 'a':
+    principal = float(input("Please enter your loan principal:\n"))
+    num_payments = float(input("Please enter the number of payments:\n"))
+    interest_rate = float(input("Please enter your interest rate:\n"))
+    ret = monthly_payment(principal, num_payments, interest_rate)
+#    if ret > 0:
+#        raise ValueError("Check your inputs, as the result came out negative.")
+#    else:
+    print("Your monthly bill is {}".format(int(ret)))
+
+if choice.lower() == 'p':
+    num_payments = float(input("Please enter the number of payments:\n"))
+    payment_amount = float(input("Please enter your monthly payment amount:\n"))
+    interest_rate = float(input("Please enter your interest rate:\n"))
+    ret = principal_of_loan(num_payments, payment_amount, interest_rate)
+    if ret > 0:
+        raise ValueError("Check your inputs, as the result came out negative.")
+    elif ret == 0:
+        print("You're paid off!")
+    else:
+        print("The principal of your loan is {}".format(int(ret)))
